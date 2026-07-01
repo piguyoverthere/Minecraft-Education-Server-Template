ad829e0c7d1c85ce2c4deb368953d50b9f610018
piguyoverthere <xeric314@gmail.com>
2026-06-30 17:57:29 -0700

    Create Instructions.js

---
+++ Instructions.js
@@ -0,0 +1,9 @@
+/*
+Commands:
+"!bank": warp to bank
+"!spawn": warp to spawn
+"!load <structure_name>": under progress
+"!shop": warp to shop
+"!afk": warp to afk spot
+btw, I am a total amateur and prob have a bunch of errors in my code
+*/
