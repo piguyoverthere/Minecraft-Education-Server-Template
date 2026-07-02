799afefb7a699d6e81595e9012347811af0cc16d
piguyoverthere <xeric314@gmail.com>
2026-07-02 09:58:03 -0700

    Update Instructions.js

--- Instructions.js
+++ Instructions.js
@@ -5,5 +5,5 @@ Commands:
 "!load <structure_name>": under progress
 "!shop": warp to shop
 "!afk": warp to afk spot
-btw, I am a total amateur and prob have a bunch of errors in my code
+"save": save location to warp to using "!base"
 */
