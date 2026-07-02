c842ea885338802494674c4f59db4b2ad2105842
piguyoverthere <xeric314@gmail.com>
2026-07-02 08:58:36 -0700

    Update main.js
    Prob should have used a function for the anti-combat tp but I guess this works too I prob will fix later.

--- main.js
+++ main.js
@@ -1,21 +1,44 @@
-let Selected = ""
-function get_randplayer () {
-    Selected = "@r"
-}
+let savedBase: Position = null;
 player.onChat("!bank", function () {
-    player.execute(
-    "camera @s fade color 30 60 200"
-    )
+    //prevent player from warping if in combat
+    player.execute("title @s actionbar warping in 5 seconds. Stand still.")
+    let startPos = player.position();
+    let startX = Math.round(startPos.getValue(Axis.X));
+    let startY = Math.round(startPos.getValue(Axis.Y));
+    let startZ = Math.round(startPos.getValue(Axis.Z));
+    //wait
+    player.execute("title @s actionbar teleporting... 5")
     loops.pause(1000)
-    player.execute(
-    "tp @s 6 -55 -16"
-    )
-    player.execute(
-    "msg @s!: You have been teleported to the bank"
-    )
-    player.execute(
-    "playsound mob.zombie.remedy @s"
-    )
+    player.execute("title @s actionbar teleporting... 4")
+    loops.pause(1000)
+    player.execute("title @s actionbar teleporting... 3")
+    loops.pause(1000)
+    player.execute("title @s actionbar teleporting... 2")
+    loops.pause(1000)
+    player.execute("title @s actionbar teleporting... 1")
+    loops.pause(1000)
+    let currentPos = player.position();
+    let currX = Math.round(currentPos.getValue(Axis.X));
+    let currY = Math.round(currentPos.getValue(Axis.Y));
+    let currZ = Math.round(currentPos.getValue(Axis.Z));
+    //compare coords
+    if (startX === currX && startY === currY && startZ === currZ) {
+        player.execute(
+        "camera @s fade color 30 60 200"
+        )
+        loops.pause(1000)
+        player.execute(
+        "tp @s 6 -55 -16"
+        )
+        player.execute(
+        "msg @s!: You have been teleported to the bank"
+        )
+        player.execute(
+        "playsound mob.zombie.remedy @s"
+        )
+    }else{
+        player.execute("title @s actionbar You moved, teleport fail.")
+    }
 })
 player.onChat("!tpto @r", function () {
     player.execute(
@@ -25,6 +48,9 @@ player.onChat("!tpto @r", function () {
     "/title " + ("" + Selected + (" actionbar" + (" @s " + "wants to tp to you")))
     )
 })
+function get_randplayer () {
+    Selected = "@r"
+}
 player.onChat("!load pad", function () {
     if (true) {
         player.execute(
@@ -43,63 +69,211 @@ player.onChat("!load pad", function () {
     }
 })
 player.onChat("!spawn", function () {
+    //prevent player from warping if in combat
+    player.execute("title @s actionbar warping in 5 seconds. Stand still.")
+    let startPos = player.position();
+    let startX = Math.round(startPos.getValue(Axis.X));
+    let startY = Math.round(startPos.getValue(Axis.Y));
+    let startZ = Math.round(startPos.getValue(Axis.Z));
+    //wait
+    player.execute("title @s actionbar teleporting... 5")
+    loops.pause(1000)
+    player.execute("title @s actionbar teleporting... 4")
+    loops.pause(1000)
+    player.execute("title @s actionbar teleporting... 3")
+    loops.pause(1000)
+    player.execute("title @s actionbar teleporting... 2")
+    loops.pause(1000)
+    player.execute("title @s actionbar teleporting... 1")
+    loops.pause(1000)
+    let currentPos = player.position();
+    let currX = Math.round(currentPos.getValue(Axis.X));
+    let currY = Math.round(currentPos.getValue(Axis.Y));
+    let currZ = Math.round(currentPos.getValue(Axis.Z));
+    //compare coords
+    if (startX === currX && startY === currY && startZ === currZ) {
+        player.execute(
+        "camera @s fade color 30 60 200"
+        )
+        loops.pause(1000)
+        player.execute(
+        "tp @s 15 -54 -10 facing @e[type=armor_stand, tag=tp]"
+        )
+        player.execute(
+        "msg @s !: You have been teleported to spawn\""
+        )
+        player.execute(
+        "playsound mob.zombie.remedy @s"
+        )
+    }else{
+        player.execute("You moved, teleport fail")
+    }
+})
+//IMPORTANT!!
+player.onChat("!save", function () {
+    //save the player pos to variable
+    savedBase = player.position();
+
+    //send debug message
+    player.execute("msg @s Location saved");
+})
+player.onChat("!base", function () {
+    //check if a base has been saved yet
+    if (savedBase != null) {
+        //prevent player from warping if in combat
+        player.execute("title @s actionbar warping in 5 seconds. Stand still.")
+        let startPos = player.position();
+        let startX = Math.round(startPos.getValue(Axis.X));
+        let startY = Math.round(startPos.getValue(Axis.Y));
+        let startZ = Math.round(startPos.getValue(Axis.Z));
+        //wait
+        player.execute("title @s actionbar teleporting... 5")
+        loops.pause(1000)
+        player.execute("title @s actionbar teleporting... 4")
+        loops.pause(1000)
+        player.execute("title @s actionbar teleporting... 3")
+        loops.pause(1000)
+        player.execute("title @s actionbar teleporting... 2")
+        loops.pause(1000)
+        player.execute("title @s actionbar teleporting... 1")
+        loops.pause(1000)
+        let currentPos = player.position();
+        let currX = Math.round(currentPos.getValue(Axis.X));
+        let currY = Math.round(currentPos.getValue(Axis.Y));
+        let currZ = Math.round(currentPos.getValue(Axis.Z));
+        //compare coords
+        if (startX === currX && startY === currY && startZ === currZ) {
+            player.execute("camera @s fade color 0 0 0") //rgb value
+            loops.pause(1000)
+            player.execute("playsound mob.zombie.remedy @s")
+            player.teleport(savedBase);
+        } else {
+            player.execute("title @s actionbar \(\S\S\)l\(\S\S\)4 Warp fail, you moved.")
+        }
+    } else {
+        player.execute("title @s actionbar You do not have a saved location. Use !save to save a location.");
+    }
+})
+player.onChat("!tpr", function () {
     player.execute(
     "camera @s fade color 30 60 200"
     )
     loops.pause(1000)
+    player.teleport(positions.groundPosition(randpos(
+    pos(-9999, 200, -9999),
+    pos(9999, 200, 9999)
+    )))
     player.execute(
-    "tp @s 15 -54 -10 facing @e[type=armor_stand, tag=tp]"
-    )
-    player.execute(
-    "msg @s !: You have been teleported to spawn\""
+    "msg @s !: You have been teleported to a random location. Use !spawn to return."
     )
     player.execute(
     "playsound mob.zombie.remedy @s"
     )
 })
 player.onChat("!afk", function () {
+    //tp the player to the saved coordinates
+    //prevent player from warping if in combat
+    player.execute("title @s actionbar warping in 5 seconds. Stand still.")
+    let startPos = player.position();
+    let startX = Math.round(startPos.getValue(Axis.X));
+    let startY = Math.round(startPos.getValue(Axis.Y));
+    let startZ = Math.round(startPos.getValue(Axis.Z));
+    //wait
+    player.execute("title @s actionbar teleporting... 5")
+    loops.pause(1000)
+    player.execute("title @s actionbar teleporting... 4")
+    loops.pause(1000)
+    player.execute("title @s actionbar teleporting... 3")
+    loops.pause(1000)
+    player.execute("title @s actionbar teleporting... 2")
+    loops.pause(1000)
+    player.execute("title @s actionbar teleporting... 1")
+    loops.pause(1000)
+    let currentPos = player.position();
+    let currX = Math.round(currentPos.getValue(Axis.X));
+    let currY = Math.round(currentPos.getValue(Axis.Y));
+    let currZ = Math.round(currentPos.getValue(Axis.Z));
+    //compare coords
+    if (startX === currX && startY === currY && startZ === currZ) {
+        player.execute(
+        "camera @s fade color 30 60 200"
+        )
+        loops.pause(1000)
+        player.execute(
+        "tp @s -3 -55 -16"
+        )
+        player.execute(
+        "msg @s !: You have been teleported to afk box\""
+        )
+        player.execute(
+        "playsound mob.zombie.remedy @s"
+        )
+    }else {
+        player.execute("title @s actionbar You moved, teleport fail")
+    }
+})
+player.onChat("!bot", function () {
     player.execute(
     "camera @s fade color 30 60 200"
     )
     loops.pause(1000)
+    player.teleport(positions.groundPosition(randpos(
+    pos(-9999, 200, -9999),
+    pos(9999, 200, 9999)
+    )))
     player.execute(
-    "tp @s -3 -55 -16"
-    )
-    player.execute(
-    "msg @s !: You have been teleported to afk box\""
+    "msg @s !: You have been teleported to a random location. Use !spawn to return."
     )
     player.execute(
     "playsound mob.zombie.remedy @s"
     )
 })
 player.onChat("!shop", function () {
-    player.execute(
-    "camera @s fade color 30 60 200"
-    )
+    //tp the player to the saved coordinates
+    //prevent player from warping if in combat
+    player.execute("title @s actionbar warping in 5 seconds. Stand still.")
+    let startPos = player.position();
+    let startX = Math.round(startPos.getValue(Axis.X));
+    let startY = Math.round(startPos.getValue(Axis.Y));
+    let startZ = Math.round(startPos.getValue(Axis.Z));
+    //wait
+    player.execute("title @s actionbar teleporting... 5")
     loops.pause(1000)
-    player.execute(
-    "tp @s 13 -55 9 facing @e[type=armor_stand,tag=shoptp]"
-    )
-    player.execute(
-    "msg @s !: You have been teleported to the shop\""
-    )
-    player.execute(
-    "playsound mob.zombie.remedy @s"
-    )
+    player.execute("title @s actionbar teleporting... 4")
+    loops.pause(1000)
+    player.execute("title @s actionbar teleporting... 3")
+    loops.pause(1000)
+    player.execute("title @s actionbar teleporting... 2")
+    loops.pause(1000)
+    player.execute("title @s actionbar teleporting... 1")
+    loops.pause(1000)
+    let currentPos = player.position();
+    let currX = Math.round(currentPos.getValue(Axis.X));
+    let currY = Math.round(currentPos.getValue(Axis.Y));
+    let currZ = Math.round(currentPos.getValue(Axis.Z));
+    //compare coords
+    if (startX === currX && startY === currY && startZ === currZ) {
+    
+        player.execute(
+        "camera @s fade color 30 60 200"
+        )
+        loops.pause(1000)
+        player.execute(
+        "tp @s 13 -55 9"
+        )
+        player.execute(
+        "msg @s !: You have been teleported to the shop\""
+        )
+        player.execute(
+        "playsound mob.zombie.remedy @s"
+        )
+    }else{
+        player.execute("title @s actionbar You moved, teleport fail")
+    }
 })
-//for some reason there is no boolean for isTag
-player.onChat("!load castle", function () {
-    // 1. REPLACE 100 64 100 with the exact X Y Z coordinates of your hidden Structure Block!
-    // This line places a redstone block next to it ONLY if you have the VIP tag and 50+ potatoes
-    player.execute("execute @p[tag=VIP,scores={potato=50..}] ~ ~ ~ setblock -27 -59 38 redstone_block")
-
-    // 2. Play your sound effect and deduct the potato points safely
-    player.execute("execute @p[tag=VIP,scores={potato=50..}] ~ ~ ~ playsound mob.zombie.remedy @p")
-    player.execute("execute @p[tag=VIP,scores={potato=50..}] ~ ~ ~ scoreboard players remove @p potato 50")
-
-    // 3. Instantly clear the redstone block away so the machine can reset for next time
-    player.execute("execute @p ~ ~ ~ setblock -27 -59 38 air")
-
-
-
-})
+let list = 0
+let Selected = ""
+let lastz3 = 0
+let lasty3 = 0
+let lastx3 = 0
+let player_pos = player.position()
